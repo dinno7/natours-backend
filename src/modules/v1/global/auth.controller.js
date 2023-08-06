e54de02562e2503b3ac1f112186a1d5c0cda6409
Taha dlrb <tahadlrb7@gmail.com>
2023-08-06 13:10:08 +0330

    Add login controller

--- src/modules/v1/global/auth.controller.js
+++ src/modules/v1/global/auth.controller.js
@@ -31,5 +31,30 @@ module.exports = {
         user: newUser
       }
     });
+  },
+  login: async function(req, res, next) {
+    const { email, password } = req.body;
+
+    // Check email and password that came from front-end
+    if (!email || !password)
+      return next(new AppError('Please provide email and password', 400));
+
+    // Check is user exist and password is correct
+    const user = await User.findOne({ email }).select('+password');
+    if (!user || !(await user.isPasswordCorrect(password, user.password)))
+      return next(new AppError('Incorrect email or password', 401));
+
+    // Remove password from response and generate token then send them as a response
+    user.password = undefined;
+    const token = generateJWT(user._id);
+
+    res.send({
+      ok: true,
+      status: 'success',
+      token,
+      data: {
+        user
+      }
+    });
   }
 };
