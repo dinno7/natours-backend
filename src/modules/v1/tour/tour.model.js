46c5373ef3fbf09e3455451fb7af74428c5129f7
Taha dlrb <tahadlrb7@gmail.com>
2023-08-06 13:08:45 +0330

    Add some schema middleware to tour schema

--- src/modules/v1/tour/tour.model.js
+++ src/modules/v1/tour/tour.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -9,7 +10,6 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       maxlength: [40, 'A tour name must have less or equal then 40 characters'],
       minlength: [10, 'A tour name must have more or equal then 10 characters']
-      // validate: [validator.isAlpha, 'Tour name must only contain characters']
     },
     slug: String,
     duration: {
@@ -77,6 +77,17 @@ const tourSchema = new mongoose.Schema(
 
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
+});
+
+// >> Just in .save() and create(...)
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true, trim: true });
+  next();
+});
+
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: { $ne: true } });
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
