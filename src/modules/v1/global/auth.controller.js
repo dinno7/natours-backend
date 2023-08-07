93508c776875355e3c8a8125ace23b3269aa6af9
Taha dlrb <tahadlrb7@gmail.com>
2023-08-07 22:34:48 +0330

    Add route protection login in auth controller

--- src/modules/v1/global/auth.controller.js
+++ src/modules/v1/global/auth.controller.js
@@ -1,8 +1,9 @@
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+const { promisify } = require('util');
 
 const User = require('../user/user.model');
-const { AppError } = require('../../../utils');
+const { AppError, catchError } = require('../../../utils');
+const { sendEmail } = require('./email.controller');
 
 // >> Utils functions:
 const generateJWT = id =>
@@ -12,7 +13,7 @@ const generateJWT = id =>
 
 // >> Controllers:
 module.exports = {
-  singUp: async function(req, res, next) {
+  singUp: catchError(async function(req, res, next) {
     const { name, email, password, passwordConfirm } = req.body;
     const newUser = await User.create({
       name,
@@ -22,7 +23,7 @@ module.exports = {
     });
 
     const token = generateJWT(newUser._id);
-
+    newUser.password = undefined;
     res.send({
       ok: true,
       status: 'success',
@@ -31,13 +32,15 @@ module.exports = {
         user: newUser
       }
     });
-  },
-  login: async function(req, res, next) {
+  }),
+
+  login: catchError(async function(req, res, next) {
     const { email, password } = req.body;
 
     // Check email and password that came from front-end
-    if (!email || !password)
+    if (!email || !password) {
       return next(new AppError('Please provide email and password', 400));
+    }
 
     // Check is user exist and password is correct
     const user = await User.findOne({ email }).select('+password');
@@ -56,5 +59,46 @@ module.exports = {
         user
       }
     });
-  }
+  }),
+
+  protect: catchError(async function(req, res, next) {
+    // 1) Getting token and check of it's there
+    const { authorization } = req.headers;
+    let token = null;
+    if (authorization && authorization.startsWith('Bearer')) {
+      token = authorization.split(' ').at(-1);
+    }
+    if (!token)
+      return next(
+        new AppError('You are not logged in, please login to get access'),
+        401
+      );
+
+    // 2) Verification token
+    const decoded = await promisify(jwt.verify)(
+      token,
+      process.env.JWT_SECRET_64
+    );
+
+    // 3) Check if user still exist
+    const currentUser = await User.findById(decoded.id).select([
+      '-__v',
+      '+password'
+    ]);
+    if (!currentUser)
+      return next(
+        new AppError(
+          'The user related by this token is not exist, please signup or login',
+          401
+        )
+      );
+
+    // 4) Check if user changed password after the token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat))
+      return next(
+        new AppError('User changed password recently, please login again', 401)
+      );
+    req.user = currentUser;
+    next();
+  })
 };
