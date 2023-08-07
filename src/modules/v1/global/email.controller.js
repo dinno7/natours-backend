ac4fcd24f0c357a015dfbd213620d979d352f5fd
Taha dlrb <tahadlrb7@gmail.com>
2023-08-07 22:36:30 +0330

    Install nodemailer and create email controller

---
+++ src/modules/v1/global/email.controller.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+
+module.exports = {
+  sendEmail: async options => {
+    // 1) Create transporter
+    const transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
+
+    // 2) Define email options
+    const mailOptions = {
+      from: options.from || 'Taha dlrb <tahadlrb7@gmail.com>',
+      to: options.email,
+      subject: options.subject,
+      text: options.message
+      // html:
+    };
+
+    // 3) Send the email
+    await transporter.sendMail(mailOptions);
+  }
+};
