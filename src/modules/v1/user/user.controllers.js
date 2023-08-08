fb8854ae7e710bd91f98853e0a55f524a6458dbd
Taha dlrb <tahadlrb7@gmail.com>
2023-08-08 15:00:36 +0330

    Mirror change

--- src/modules/v1/user/user.controllers.js
+++ src/modules/v1/user/user.controllers.js
@@ -1,62 +1,72 @@
 const fs = require('fs');
 const { uuidv4 } = require('./user.utils');
+const User = require('./user.model');
 
 const usersFilePath = `${__dirname}/../../../static/data/users.json`;
 const users = JSON.parse(fs.readFileSync(usersFilePath));
 
 module.exports = {
-	getAllUsers: function (req, res) {
-		return res.status(200).send({
-			ok: true,
-			result: users.length,
-			data: {
-				users,
-			},
-		});
-	},
+  getAllUsers: async function(req, res) {
+    const users = await User.find();
 
-	getUserById: (req, res) => {
-		return res.status(200).send({
-			ok: true,
-			result: 1,
-			data: {
-				user: req.foundUser,
-			},
-		});
-	},
+    return res.status(200).send({
+      ok: true,
+      result: users.length,
+      data: {
+        users
+      }
+    });
+  },
 
-	createUser: (req, res) => {
-		let newUser = req.body;
-		if (!newUser || !Object.keys(newUser).length)
-			return res.status(401).send({
-				ok: false,
-				result: 0,
-				message: 'User can not be empty.',
-			});
-		const userId = uuidv4();
+  getUserById: (req, res) => {
+    return res.status(200).send({
+      ok: true,
+      result: 1,
+      data: {
+        user: req.foundUser
+      }
+    });
+  },
 
-		newUser = { id: userId, ...newUser };
+  createUser: (req, res) => {
+    let newUser = req.body;
+    if (!newUser || !Object.keys(newUser).length)
+      return res.status(401).send({
+        ok: false,
+        result: 0,
+        message: 'User can not be empty.'
+      });
+    const userId = uuidv4();
 
-		users.push(newUser);
-		fs.writeFile(usersFilePath, JSON.stringify(users), (err) => {
-			if (err) {
-				return res.status(401).send({ ok: false, message: err.message || 'There is some problem in creating new user.' });
-			}
+    newUser = { id: userId, ...newUser };
 
-			return res.status(201).send({
-				ok: true,
-				result: 1,
-				data: {
-					user: newUser,
-				},
-			});
-		});
-	},
+    users.push(newUser);
+    fs.writeFile(usersFilePath, JSON.stringify(users), err => {
+      if (err) {
+        return res
+          .status(401)
+          .send({
+            ok: false,
+            message:
+              err.message || 'There is some problem in creating new user.'
+          });
+      }
 
-	checkUserId: function (req, res, next, userId) {
-		let user = users.find((t) => t._id == userId);
-		if (!user) return res.status(404).send({ ok: false, message: 'User not found!' });
-		req.foundUser = user;
-		return next();
-	},
+      return res.status(201).send({
+        ok: true,
+        result: 1,
+        data: {
+          user: newUser
+        }
+      });
+    });
+  },
+
+  checkUserId: function(req, res, next, userId) {
+    let user = users.find(t => t._id == userId);
+    if (!user)
+      return res.status(404).send({ ok: false, message: 'User not found!' });
+    req.foundUser = user;
+    return next();
+  }
 };
