033605df30b31eb5ebaa5c5e970897ba7cb6df53
Taha dlrb <tahadlrb7@gmail.com>
2023-08-23 16:10:07 +0330

    Add calculate ratings params in tour when review change

--- src/modules/v1/review/review.model.js
+++ src/modules/v1/review/review.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('../tour/tour.model');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -34,6 +35,43 @@ reviewSchema.pre(/^find/, function(next) {
   this.populate({ path: 'user', select: 'name' });
   next();
 });
+
+// Calculate average rating in tour after create new review
+reviewSchema.post('save', async function(savedDoc, next) {
+  if (!savedDoc) return next();
+  await savedDoc.constructor.calcAverageRating(savedDoc.tour);
+  next();
+});
+
+// Calculate average rating in tour after update and delete review
+reviewSchema.post(/^findOneAnd/, async function(res, next) {
+  if (!res) return next();
+  await res.constructor.calcAverageRating(res.tour);
+  next();
+});
+
+// Save ratings avg and rating quantity in tour model (for after create new review)
+reviewSchema.statics.calcAverageRating = async function(tourId) {
+  const stats = await this.aggregate([
+    { $match: { tour: tourId } },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        nAvgRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+  let ratingsUpdate = {
+    ratingsAverage: 0,
+    ratingsQuantity: 0
+  };
+  if (stats.length > 0) {
+    ratingsUpdate.ratingsQuantity = stats[0].nRating;
+    ratingsUpdate.ratingsAverage = stats[0].nAvgRating;
+  }
+  await Tour.findByIdAndUpdate(tourId, ratingsUpdate);
+};
 
 const reviewModel = mongoose.model('Review', reviewSchema);
 
